b0fee6cd7ff5206c619bb16491e96f3788f03dd7
arzu <arzuagamirzaeva2006@gmail.com>
2025-07-08 18:41:27 +0400

    Add files via upload
    Qiyməti 2 AZN-dən az olan məhsulları göstər.
    
    Miqdarı (Quantity) 5 və daha çox olan sifarişləri seç.
    
    Məhsulu “Kitab” olan bütün sifarişləri göstər.
    
    Müştəri adı “Leyla Məmmədova” olan sifarişləri süz.
    
    Tarixi 2025-06-05 və ondan sonra olan sifarişləri göstər.
    
    Müştəri adında “Arzu” keçən bütün sifarişləri seç.

---
+++ sql(task).sql
@@ -0,0 +1,28 @@
+﻿Select * From Orders
+
+
+Select * From Orders
+Where Price<2
+
+
+
+Select * From Orders
+Where Quantity>=5
+
+
+Select * From Orders
+Where Product = 'Kitab'
+
+
+
+Select * From Orders
+Where CustomerName='Leyla Məmmədova'
+
+
+
+Select * From Orders
+Where Orderdate>='2025-06-05'
+
+
+Select * From Orders
+Where CustomerName LIKE '%Arzu%'
